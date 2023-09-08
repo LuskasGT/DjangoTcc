32dfc7105742ca20ef04137bc052316a4e2e78e3
Lucas GT <lucas.g.theodooro@gmail.com>
2023-09-08 11:58:24 -0300

    criando a view e a url do fale conosco

--- itapira/itapira/urls.py
+++ itapira/itapira/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from tcc import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
+    path ('',views.index,name="index"),
+    path ('fale-conosco', views.faleConosco, name="faleConosco"),
+    path ('adote')
 ]
--- itapira/tcc/views.py
+++ itapira/tcc/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'tcc/index.html')
+
+def faleConosco(request):
+    return render(request,'tcc/fale-conosco.html')
